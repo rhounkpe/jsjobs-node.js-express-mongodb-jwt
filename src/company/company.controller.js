e20794da3058daaf63a1ea407fcedcd259f5b67a
Rufin Hounkpe <rhounkpe@gmail.com>
2019-11-14 15:06:41 +0100

    Validating schemas: not really working not

--- src/company/company.controller.js
+++ src/company/company.controller.js
@@ -3,7 +3,10 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
+const { check, validationResult, checkSchema } = require('express-validator');
+
 const {Company, companySchema} = require('../company/company.model');
+const { companyRegistrationSchema } = require('./validation.schemas');
 
 exports.login = async (req, res) => {
   try {
@@ -47,35 +50,58 @@ exports.login = async (req, res) => {
   }
 };
 
-exports.register = (req, res) => {
-  if (req.body) {
-    const email = req.body.email.trim();
-    const password = req.body.password.trim();
-    const nickname = req.body.nickname.trim();
 
-    const company = new Company({
-      name: nickname,
-      email: email,
-      password: password
-    });
+const errorFormatter = ({ location, msg, param, value, nestedErrors }) => {
+  // Build your resulting errors however you want! String, object, whatever - it works!
+  return `${location}[${param}]: ${msg}`;
+};
 
-    company.save((err, comp) => {
-      if (err) {
-        return res.status(400).send({
-          message: `Error: ${err.stack}`
+exports.register = async (req, res) => {
+  try {
+    // const Company = await getCompanyModel();
+
+    checkSchema(companyRegistrationSchema) ;
+    const errors = validationResult(req).formatWith(errorFormatter);
+    if (!errors.isEmpty()) {
+      return res.status(500).json(`There is an error: ${errors.array()}`);
+    }
+
+    if (req.body) {
+      const email = req.body.email.trim();
+      const password = req.body.password.trim();
+      const nickname = req.body.nickname.trim();
+
+      const submittedCompany = {
+        name: nickname,
+        email: email,
+        password: password
+      };
+
+      const company = new Company(submittedCompany);
+
+      await company.save()
+        .then(function (company) {
+          if (company) {
+            console.log(`Company newly created: ${JSON.stringify(company)}`);
+          }
+        })
+        .catch(function (err) {
+          console.error(`An error happened when creating new comapany: ${err.stack}`);
+          return res.status(400).send({message: `Error: ${err.stack}`});
         });
-      } else {
-        res.json({
-          success: true,
-          company: comp
-        });
-      }
-    });
-  } else {
-    res.json({
-      success: false,
-      message: 'Echec de la création de une nouvelle compagnie',
-    });
+
+      res.status(201).json(company);
+
+    } else {
+      res.json({
+        success: false,
+        message: 'Echec de la création de une nouvelle compagnie',
+      });
+    }
+
+  }catch (e) {
+    console.error(`An error happened when creating a new company: ${e.stack}`);
   }
+
 };
 
---
+++ src/company/validation.schemas.js
@@ -0,0 +1,22 @@
+'use strict';
+
+exports.companyRegistrationSchema = {
+  "email": {
+    //notEmpty: true,
+    isEmail: {
+      errorMessage: "Invalid Email"
+    }
+  },
+  "password": {
+    //notEmpty: true,
+    isLength: {
+      options: [{min: 12}],
+      errorMessage: "Password must be at least 12 characters."
+    },
+    matches: {
+      options: ["(?=.*[a-zA-Z])(?=.*[0-9]+)(?=.*[!*&^%$#@()+]+).*", "g"],
+      errorMessage: "Password must be alphanumeric."
+    },
+    errorMessage: "Invalid password"
+  }
+};
