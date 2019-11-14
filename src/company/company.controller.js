ee41f7dc8e34041d6745de31eb7f4a5eb168ebda
Rufin Hounkpe <rhounkpe@gmail.com>
2019-11-14 13:36:37 +0100

    Refactoring company login controller

--- src/company/company.controller.js
+++ src/company/company.controller.js
@@ -9,40 +9,39 @@ exports.login = async (req, res) => {
   try {
     //const Company = await getCompanyModel();
     const { email, password } = req.body;
-    const existingCompany = await Company.findOne({email: email}).exec();
-
-    console.log(`existingCompany: ${JSON.stringify(existingCompany)}`);
-
-    if (!existingCompany) {
-      return res.status(401).send('Invalid email or password for a company');
-    }
-
-    existingCompany.passwordIsValid(password, function (err, results) {
+    await Company.findOne({email: email}).exec((err, existingCompany) => {
       if (err) {
-        return res.status(500).send('N° 1 | There is a problem logging in at the moment. Please try again later.');
-      } else if (!results) {
-        return res.status(401).send('Invalid email or password');
+        return res.status(401).send('Invalid email or password for a company');
+      } else {
+        existingCompany.passwordIsValid(password, function (err, results) {
+          if (err) {
+            return res.status(500).send('N° 1 | There is a problem logging in at the moment. Please try again later.');
+          } else if (!results) {
+            return res.status(401).send('Invalid email or password');
+          }
+
+          const companyInfo = {
+            _id: existingCompany._id,
+            email: existingCompany.email,
+            nickname: existingCompany.nickname
+          };
+
+          // req.session.login(companyInfo);
+          const token = jwt.sign({
+            iss: config.jwt.issuer,
+            role: existingCompany.role,
+            email: existingCompany.email,
+            companyId: existingCompany.id,
+          }, config.jwt.secret);
+
+          return res.status(200).json({
+            company: existingCompany,
+            token,
+          });
+        });
       }
-
-      const companyInfo = {
-        _id: existingCompany._id,
-        email: existingCompany.email,
-        nickname: existingCompany.nickname
-      };
-
-      // req.session.login(companyInfo);
-      const token = jwt.sign({
-        iss: config.jwt.issuer,
-        role: existingCompany.role,
-        email: existingCompany.email,
-        companyId: existingCompany.id,
-      }, config.jwt.secret);
-
-      return res.status(200).json({
-        company: existingCompany,
-        token,
-      });
     });
+
   } catch (err) {
     return res.status(500).send('There is a problem logging in at the moment. Please try again later.');
   }
